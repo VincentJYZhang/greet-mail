dbcf4b370cfad48f81a8e240bafe70804ba37a2d
VincentJYZhang <vincentzjy@outlook.com>
2018-09-18 00:44:48 +0800

    __main__

---
+++ src/__main__.py
@@ -0,0 +1,13 @@
+"""
+    Create by Vincent Zhang
+    on 2018.09.13
+    GitHub: https://github.com/VincentJYZhang/greet-mail
+"""
+
+__author__ = 'Vincent Zhang'
+
+import app
+
+
+if __name__ == '__main__':
+    app.start()
